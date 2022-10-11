4394f1f75f994bfacbcabd04ded0ee6333454d31
thomaslepoix <thomas.lepoix@protonmail.ch>
2022-10-11 04:17:06 +0200

    add & test MeshlinePolicyManager::detect_intervals() + MeshlinePolicyManager::mesh() + GridAxis cast(MeshlinePolicy::Axis)

--- test/unit/domain/test_meshline_policy_manager.cpp
+++ test/unit/domain/test_meshline_policy_manager.cpp
@@ -15,6 +15,7 @@
 #include "domain/meshline_policy_manager.hpp"
 
 /// @test GridAxis cast(MeshlinePolicy::Axis const a)
+/// @test Interval::Axis cast(GridAxis const a)
 /// @test MeshlinePolicy* MeshlinePolicyManager::add_meshline_policy(
 ///       	IMeshLineOrigin* origin,
 ///       	MeshlinePolicy::Axis const axis,
@@ -26,6 +27,8 @@
 ///       	vector<MeshlinePolicy*> dimension,
 ///       	Coord proximity_limit)
 /// @test void MeshlinePolicyManager::detect_and_solve_too_close_meshline_policies()
+/// @test void MeshlinePolicyManager::detect_intervals()
+/// @test void MeshlinePolicyManager::mesh()
 ///*****************************************************************************
 
 //******************************************************************************
@@ -37,10 +40,28 @@ SCENARIO("GridAxis cast(MeshlinePolicy::Axis const a)", "[meshline_policy_manage
 				REQUIRE(a == H);
 			}
 		}
-		WHEN("a == MeshlinePolicy::Axis::V") {
+		WHEN("MeshlinePolicy::Axis::V") {
 			GridAxis a = cast(MeshlinePolicy::Axis::V);
 			THEN("Should return V") {
 				REQUIRE(a == V);
+			}
+		}
+	}
+}
+
+//******************************************************************************
+SCENARIO("Interval::Axis cast(GridAxis const a)", "[meshline_policy_manager]") {
+	GIVEN("A GridAxis enum") {
+		WHEN("H") {
+			Interval::Axis a = cast(H);
+			THEN("Should return Interval::Axis::H") {
+				REQUIRE(a == Interval::Axis::H);
+			}
+		}
+		WHEN("V") {
+			Interval::Axis a = cast(V);
+			THEN("Should return V") {
+				REQUIRE(a == Interval::Axis::V);
 			}
 		}
 	}
@@ -418,6 +439,204 @@ SCENARIO("void MeshlinePolicyManager::detect_and_solve_too_close_meshline_polici
 			REQUIRE(w.mpm.line_policies[H][6]->is_enabled);
 			REQUIRE(w.mpm.line_policies[H][7]->coord == 11.4);
 			REQUIRE(w.mpm.line_policies[H][7]->is_enabled);
+		}
+	}
+}
+
+//******************************************************************************
+SCENARIO("void MeshlinePolicyManager::detect_intervals()", "[meshline_policy_manager]") {
+	class Wrapper {
+	public:
+		Params params;
+		ConflictManager cm;
+		MeshlinePolicyManager mpm;
+
+		Wrapper()
+		: cm(&mpm)
+		, mpm(params, &cm)
+		{}
+	};
+
+	GIVEN("A meshline policy manager and some meshline policies") {
+		Wrapper w;
+		w.params.proximity_limit = 1;
+		Point e0(1, 1), e1(1, 3);
+		Edge e(&e0, &e1);
+
+		w.mpm.add_meshline_policy(
+			&e,
+			MeshlinePolicy::Axis::H,
+			MeshlinePolicy::Policy::HALFS,
+			Normal::NONE,
+			10);
+		w.mpm.add_meshline_policy(
+			&e,
+			MeshlinePolicy::Axis::H,
+			MeshlinePolicy::Policy::HALFS,
+			Normal::NONE,
+			40);
+		w.mpm.add_meshline_policy(
+			&e,
+			MeshlinePolicy::Axis::H,
+			MeshlinePolicy::Policy::HALFS,
+			Normal::NONE,
+			30);
+		w.mpm.add_meshline_policy(
+			&e,
+			MeshlinePolicy::Axis::H,
+			MeshlinePolicy::Policy::HALFS,
+			Normal::NONE,
+			20);
+		w.mpm.add_meshline_policy(
+			&e,
+			MeshlinePolicy::Axis::V,
+			MeshlinePolicy::Policy::HALFS,
+			Normal::NONE,
+			30);
+		w.mpm.add_meshline_policy(
+			&e,
+			MeshlinePolicy::Axis::V,
+			MeshlinePolicy::Policy::HALFS,
+			Normal::NONE,
+			25,
+			false);
+		w.mpm.add_meshline_policy(
+			&e,
+			MeshlinePolicy::Axis::V,
+			MeshlinePolicy::Policy::HALFS,
+			Normal::NONE,
+			20);
+
+		THEN("Intervals betwen each adjacent meshline policies should be detected and ordered") {
+			w.mpm.detect_intervals();
+			REQUIRE(w.mpm.intervals[H].size() == 3);
+			REQUIRE(w.mpm.intervals[V].size() == 1);
+			REQUIRE(w.mpm.intervals[H][0]->before.meshline_policy == w.mpm.line_policies[H][0].get());
+			REQUIRE(w.mpm.intervals[H][0]->after.meshline_policy == w.mpm.line_policies[H][3].get());
+			REQUIRE(w.mpm.intervals[H][1]->before.meshline_policy == w.mpm.line_policies[H][3].get());
+			REQUIRE(w.mpm.intervals[H][1]->after.meshline_policy == w.mpm.line_policies[H][2].get());
+			REQUIRE(w.mpm.intervals[H][2]->before.meshline_policy == w.mpm.line_policies[H][2].get());
+			REQUIRE(w.mpm.intervals[H][2]->after.meshline_policy == w.mpm.line_policies[H][1].get());
+			REQUIRE(w.mpm.intervals[V][0]->before.meshline_policy == w.mpm.line_policies[V][2].get());
+			REQUIRE(w.mpm.intervals[V][0]->after.meshline_policy == w.mpm.line_policies[V][0].get());
+			REQUIRE(w.mpm.intervals[H][0]->m.value() == 15);
+			REQUIRE(w.mpm.intervals[H][1]->m.value() == 25);
+			REQUIRE(w.mpm.intervals[H][2]->m.value() == 35);
+			REQUIRE(w.mpm.intervals[V][0]->m.value() == 25);
+			REQUIRE(w.mpm.intervals[H][0]->h.value() == 5);
+			REQUIRE(w.mpm.intervals[H][1]->h.value() == 5);
+			REQUIRE(w.mpm.intervals[H][2]->h.value() == 5);
+			REQUIRE(w.mpm.intervals[V][0]->h.value() == 5);
+
+		}
+	}
+}
+
+//******************************************************************************
+SCENARIO("void MeshlinePolicyManager::mesh()", "[meshline_policy_manager]") {
+	class Wrapper {
+	public:
+		Params params;
+		ConflictManager cm;
+		MeshlinePolicyManager mpm;
+
+		Wrapper()
+		: cm(&mpm)
+		, mpm(params, &cm)
+		{}
+	};
+
+	GIVEN("A meshline policy manager and some meshline policies") {
+		Wrapper w;
+		w.params.proximity_limit = 1;
+		w.params.lmin = 2;
+		w.params.dmax = 4.0;
+		Point e0(1, 1), e1(1, 3);
+		Edge e(&e0, &e1);
+
+		w.mpm.add_meshline_policy(
+			&e,
+			MeshlinePolicy::Axis::H,
+			MeshlinePolicy::Policy::HALFS,
+			Normal::NONE,
+			10);
+		w.mpm.add_meshline_policy(
+			&e,
+			MeshlinePolicy::Axis::H,
+			MeshlinePolicy::Policy::ONELINE,
+			Normal::NONE,
+			40);
+		w.mpm.add_meshline_policy(
+			&e,
+			MeshlinePolicy::Axis::H,
+			MeshlinePolicy::Policy::THIRDS,
+			Normal::YMAX,
+			30);
+		w.mpm.add_meshline_policy(
+			&e,
+			MeshlinePolicy::Axis::H,
+			MeshlinePolicy::Policy::THIRDS,
+			Normal::YMIN,
+			25);
+		w.mpm.add_meshline_policy(
+			&e,
+			MeshlinePolicy::Axis::H,
+			MeshlinePolicy::Policy::ONELINE,
+			Normal::NONE,
+			25.1);
+
+		w.mpm.detect_intervals();
+		w.mpm.mesh();
+
+		THEN("Meshlines should be sorted") {
+			REQUIRE(w.mpm.meshlines[H].size() > 2);
+			for(auto it = next(begin(w.mpm.meshlines[H])); it != end(w.mpm.meshlines[H]); ++it)
+				REQUIRE((*prev(it))->coord < (*it)->coord);
+		}
+
+		THEN("ONELINE meshlines should be placed precisely") {
+			auto does_contains = [&w](Coord a) -> bool {
+						for(auto const& it : w.mpm.meshlines[H])
+							if(it->coord == a)
+								return true;
+						return false;
+					};
+
+			REQUIRE(does_contains(25.1));
+			REQUIRE(does_contains(40));
+		}
+
+		THEN("Every space should be thiner than dmax") {
+			REQUIRE(w.mpm.meshlines[H].size() > 2);
+			for(auto it = next(begin(w.mpm.meshlines[H])); it != end(w.mpm.meshlines[H]); ++it)
+				REQUIRE(distance((*prev(it))->coord, (*it)->coord) <= 4.0);
+		}
+
+		THEN("Every space should be [0.5; 2] times its adjacent spaces") {
+			REQUIRE(w.mpm.meshlines[H].size() > 2);
+			for(auto it = next(begin(w.mpm.meshlines[H]), 2); it != end(w.mpm.meshlines[H]); ++it) {
+				Coord a(distance((*prev(it, 2))->coord, (*prev(it))->coord));
+				Coord b(distance((*prev(it))->coord, (*it)->coord));
+				REQUIRE(a <= b * 2);
+				REQUIRE(a >= b / 2);
+			}
+		}
+
+		THEN("Every space should contain at least lmin * 2 + 1 lines") {
+			auto has_enough_lines = [&w](Coord a, Coord b, size_t lmin) -> bool {
+				Coord const c(mid(a, b));
+				size_t lines = 0;
+				for(auto const& it : w.mpm.meshlines[H])
+					if(it->coord >= a && it->coord <= b)
+						++lines;
+				UNSCOPED_INFO("lines : " << lines);
+				return lines >= lmin * 2 + 1;
+			};
+
+			REQUIRE(has_enough_lines(10, 25, 2));
+			REQUIRE(has_enough_lines(25, 25.1, 2));
+			REQUIRE(has_enough_lines(25.1, 30, 2));
+			REQUIRE(has_enough_lines(30, 40, 2));
 		}
 	}
 }
