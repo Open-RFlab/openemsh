837e9cc32307e67b13db1c07d5be8451c39a6104
tlepoix@localhost <thomas.lepoix@protonmail.ch>
2025-12-17 07:52:40 +0100

    add Logger

---
+++ src/ui/cli/logger.cpp
@@ -0,0 +1,111 @@
+///*****************************************************************************
+/// @date Feb 2021
+/// @copyright GPL-3.0-or-later
+/// @author Thomas Lepoix <thomas.lepoix@protonmail.ch>
+///*****************************************************************************
+
+#include <format>
+#include <iostream>
+#include <print>
+
+#include "utils/unreachable.hpp"
+
+#include "logger.hpp"
+
+using namespace std;
+
+namespace ui::cli {
+
+//******************************************************************************
+string to_string(Logger::Level level) noexcept {
+	switch(level) {
+	case Logger::Level::QUESTION: return "Question";
+	case Logger::Level::INFO: return "Info";
+	case Logger::Level::WARNING: return "Warning";
+	case Logger::Level::ERROR: return "Error";
+	default: ::unreachable();
+	}
+}
+
+//******************************************************************************
+string to_emoji(Logger::Level level) noexcept {
+	switch(level) {
+//	case Logger::Level::QUESTION: return "❔";
+	case Logger::Level::QUESTION: return "❓";
+	case Logger::Level::INFO: return "ℹ️ ";
+	case Logger::Level::WARNING: return "⚠️ ";
+	case Logger::Level::ERROR: return "⛔";
+//	case Logger::Level::QUESTION: return "\u2754";
+//	case Logger::Level::QUESTION: return "\u2753";
+//	case Logger::Level::INFO: return "\u2139";
+//	case Logger::Level::WARNING: return "\u26A0";
+//	case Logger::Level::ERROR: return "\u26D4";
+	default: ::unreachable();
+	}
+}
+
+//******************************************************************************
+string to_string(Logger::UserAction action) noexcept {
+	switch(action) {
+	case Logger::UserAction::NOTHING: return "";
+	case Logger::UserAction::CANCEL: return "n";
+	case Logger::UserAction::OK: return "y";
+	case Logger::UserAction::SAVE: return "s";
+	case Logger::UserAction::ABORT: return "a";
+	default: ::unreachable();
+	}
+}
+
+//******************************************************************************
+string to_string(set<Logger::UserAction> const& actions) noexcept {
+	string ret("[");
+	for(auto const action : actions)
+		ret += to_string(action) + "/";
+	if(!actions.empty())
+		ret.pop_back();
+	ret += "]";
+	return ret;
+}
+
+//******************************************************************************
+Logger::UserAction from_string(string const& str) noexcept {
+	if(str == "n") return Logger::UserAction::CANCEL;
+	else if(str == "y") return Logger::UserAction::OK;
+	else if(str == "s") return Logger::UserAction::SAVE;
+	else if(str == "a") return Logger::UserAction::ABORT;
+	else return Logger::UserAction::NOTHING;
+}
+
+//******************************************************************************
+LoggerSink::LoggerSink(bool verbose)
+: verbose(verbose)
+{}
+
+//******************************************************************************
+Logger::UserAction LoggerSink::log(Logger::LogEvent const& log) const {
+	if(!(log.level == Logger::Level::INFO && !verbose))
+		print(cerr,
+			"{} {}: {}",
+			to_emoji(log.level),
+			to_string(log.level),
+			log.message);
+
+	if(is_interactive
+	&& log.level == Logger::Level::QUESTION
+	&& !log.user_actions.empty()) {
+		Logger::UserAction res = Logger::UserAction::NOTHING;
+		do {
+			print(cerr, "{} ? ", to_string(log.user_actions));
+			string ln;
+			getline(cin, ln);
+			res = from_string(ln);
+		} while(res == Logger::UserAction::NOTHING);
+		return res;
+	} else {
+		if(!(log.level == Logger::Level::INFO && !verbose))
+			println(cerr);
+		return Logger::UserAction::NOTHING;
+	}
+}
+
+} // namespace ui::cli
---
+++ src/ui/qt/logger.cpp
@@ -0,0 +1,87 @@
+///*****************************************************************************
+/// @date Feb 2021
+/// @copyright GPL-3.0-or-later
+/// @author Thomas Lepoix <thomas.lepoix@protonmail.ch>
+///*****************************************************************************
+
+#include <QMessageBox>
+#include <QStatusBar>
+
+#include "utils/unreachable.hpp"
+
+#include "logger.hpp"
+
+using namespace std;
+
+namespace ui::qt {
+
+//******************************************************************************
+Logger::UserAction from_qt(int button) noexcept {
+	switch(static_cast<QMessageBox::StandardButton>(button)) {
+	default: [[fallthrough]];
+	case QMessageBox::NoButton: return Logger::UserAction::NOTHING;
+	case QMessageBox::Cancel: return Logger::UserAction::CANCEL;
+	case QMessageBox::Ok: return Logger::UserAction::OK;
+	case QMessageBox::Save: return Logger::UserAction::SAVE;
+	case QMessageBox::Abort: return Logger::UserAction::ABORT;
+	}
+}
+
+//******************************************************************************
+QMessageBox::StandardButton to_qt(Logger::UserAction action) noexcept {
+	switch(action) {
+	case Logger::UserAction::NOTHING: return QMessageBox::NoButton;
+	case Logger::UserAction::CANCEL: return QMessageBox::Cancel;
+	case Logger::UserAction::OK: return QMessageBox::Ok;
+	case Logger::UserAction::SAVE: return QMessageBox::Save;
+	case Logger::UserAction::ABORT: return QMessageBox::Abort;
+	default: ::unreachable();
+	}
+}
+
+//******************************************************************************
+QMessageBox::StandardButtons to_qt(set<Logger::UserAction> const& actions) noexcept {
+	QMessageBox::StandardButtons buttons = QMessageBox::NoButton;
+	for(auto const action : actions)
+		buttons |= to_qt(action);
+	return buttons;
+}
+
+//******************************************************************************
+QMessageBox::Icon to_qt(Logger::Level level) noexcept {
+	switch(level) {
+	case Logger::Level::QUESTION: return QMessageBox::Question;
+	case Logger::Level::INFO: return QMessageBox::Information;
+	case Logger::Level::WARNING: return QMessageBox::Warning;
+	case Logger::Level::ERROR: return QMessageBox::Critical;
+	default: ::unreachable();
+	}
+}
+
+//******************************************************************************
+LoggerSink::LoggerSink(QStatusBar* status_bar, QWidget* parent)
+: status_bar(status_bar)
+, parent(parent)
+{}
+
+//******************************************************************************
+Logger::UserAction LoggerSink::log(Logger::LogEvent const& log) const {
+	if(log.level != Logger::Level::QUESTION
+	&& (log.user_actions.empty() || log.user_actions.contains(Logger::UserAction::NOTHING))) {
+		status_bar->showMessage(QString::fromStdString(log.message));
+		return Logger::UserAction::NOTHING;
+	} else {
+		QMessageBox message(parent);
+		message.setText(QString::fromStdString(log.message));
+		message.setIcon(to_qt(log.level));
+		message.setStandardButtons(is_interactive
+			? to_qt(log.user_actions)
+			: QMessageBox::Ignore);
+		if(log.default_user_action.has_value())
+			message.setDefaultButton(to_qt(log.default_user_action.value()));
+
+		return from_qt(message.exec());
+	}
+}
+
+} // namespace ui::qt
